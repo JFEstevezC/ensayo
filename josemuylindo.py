61ed0338d4383c03eabebaaa6e82f2fae01deee3
JFEstevezC <jose2224646@correo.uis.edu.co>
2022-12-01 09:25:56 -0500

    Avances del proyecto

--- josemuylindo.py
+++ josemuylindo.py
@@ -1,2 +1,20 @@
 # A usted qué le tocó?
 # un programa de una matriz para buscar un numero dentro de ella 
+# Listo, nos salió el mismo
+# Generación de una matriz por indexación
+import random
+
+# Dimensiones de la matriz
+m = int(input("No. de filas de la Matriz: "))
+n = int(input("No. de columnas de la Matriz: "))
+
+# Creación de la matriz
+
+M = []
+
+for i in range(m):
+  M.append([])
+  for j in range(n):
+    M[i].append(random.randint(1,9))
+
+# creo que eso sirve un poco. Para lo de el número entero que define la matriz cuadrada
