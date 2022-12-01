b1b4b3995be5139a827f931b0134875e73e9934c
JFEstevezC <jose2224646@correo.uis.edu.co>
2022-12-01 09:18:50 -0500

    Avances del proyecto

---
+++ josemuylindo.py
@@ -0,0 +1,1 @@
+# A usted qué le tocó?
--- parcial2.py
+++ parcial2.py
@@ -1,2 +1,9 @@
-a=2
-print(a)
+def sumar(a,b):
+    z=a+b
+    print(z)
+
+a=int((input("Escriba el número 1")))
+b=int((input("Escriba el número 1")))
+
+print("la suma es: "), sumar(a,b)
+
